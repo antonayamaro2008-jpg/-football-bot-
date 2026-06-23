db3544150a9b990dff9e9088db62218af755d4c0
antonayamaro2008-jpg <antonayamaro2008@gmail.com>
2026-06-23 03:12:15 +0100

    Create telegram_bot.py

---
+++ telegram_bot.py
@@ -0,0 +1,6 @@
+import requests
+from config import BOT_TOKEN, CHAT_ID
+
+def send_message(text):
+    url = f"https://api.telegram.org/bot{8618348040:AAHVdRVjKBgFaSb7t3JGt2BKh-IVX_yVFj4}/sendMessage"
+    requests.post(url, data={"chat_id": CHAT_ID, "text": text})
