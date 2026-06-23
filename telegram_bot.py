1fe70134c27591f7283cd10cc70011274828e8ed
antonayamaro2008-jpg <antonayamaro2008@gmail.com>
2026-06-23 12:58:14 +0100

    Update telegram_bot.py

--- telegram_bot.py
+++ telegram_bot.py
@@ -2,5 +2,5 @@ import requests
 from config import BOT_TOKEN, CHAT_ID
 
 def send_message(text):
-    url = f"https://api.telegram.org/bot{8618348040:AAHVdRVjKBgFaSb7t3JGt2BKh-IVX_yVFj4}/sendMessage"
-    requests.post(url, data={"chat_id": CHAT_ID, "text": text})
+    url = f"https://api.telegram.org/bot{8603081100:AAEMHrUrQA_W6Q_YderSYgQP0TUYtmmyKHo}/sendMessage"
+    requests.post(url, data={"chat_id": 6689658340, "text": text})
